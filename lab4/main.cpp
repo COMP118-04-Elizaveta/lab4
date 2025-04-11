4109c3d138a272aabbe3f5552beeff7e95a19281
drist37 <vasilieva.e@live.unic.ac.cy>
2025-04-11 11:42:29 +0300

    Created menu and choice selection

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -20,6 +20,32 @@ int main() {
     
     double mat[MAX_ROW][MAX_COL] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; //@todo delete random values
     
+    int choice;
+    
+    do {
+        cout << "\n0) Exit";
+        cout << "\n1) Show Matrix";
+        cout << "\n2) todo";
+        cout << "\nEnter choice: ";
+        cin >> choice;
+        
+        
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                printMatrix(mat, MAX_ROW);
+                break;
+            case 2:
+                break;
+            default:
+                cout << "Invalid choice";
+                break;
+            
+        }
+        
+    } while (choice != 0);
+    
     printMatrix (mat, MAX_ROW);
     cout << "Have a nice day :3\n";
     
